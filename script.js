9baafba36a378e127420054612547140054b7daa
mitchm23 <69411959+mitchm23@users.noreply.github.com>
2020-10-31 14:55:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,88 @@
 
-$(document).ready(function(){
-    let timeArray = ["9:00AM","10:00AM","11:00AM","12:00PM","1:00PM","2:00PM","3:00PM","4:00PM","5:00PM"]
-    let now = new Date()
-    let currentDate= moment().format("MMMM Do YYYY");
-    let y = now.getFullYear()
-    let m = now.getMonth()+1;
-    let d = now.getDate()
+// onstart
 
-    let total = new Date(y,m,0).getDate()
-
-    let output =
-    (m<10 ? '0' : '') + m + '/' +
-    (d<10 ? '0' : '') + d + '/' + y
-
-    
-
-    let container = $(".container")
-    let time =$("#currentDay").text(currentDate)
-
+$(document).ready(function () {
+  let timeArray = [
+    "9:00AM",
+    "10:00AM",
+    "11:00AM",
+    "12:00PM",
+    "1:00PM",
+    "2:00PM",
+    "3:00PM",
+    "4:00PM",
+    "5:00PM",
 
     
+  ];
 
-    for(i = 0; i < timeArray.length; i++){
-        let timeBlock=$("<div>").addClass("time-block")
-        let row=$("<div>").addClass("row")
-        let hour=$("<div>").addClass("hour col-2")
-        $(hour).text(timeArray[i])
-        let past=$("<input>").addClass("past col-8")
-        let saveBtn=$("<button>").addClass("saveBtn col-2")
-        $(saveBtn).text("Save")
-        $(row).append(hour)
-        $(row).append(past)
-        $(row).append(saveBtn)
-        $(timeBlock).append(row)
-        $(container).append(timeBlock)
+ 
+//   get current time and date
+  let now = new Date();
+  let currentDate = moment().format("MMMM Do YYYY");
+  let y = now.getFullYear();
+  let m = now.getMonth() + 1;
+  let d = now.getDate();
 
-        
+  let total = new Date(y, m, 0).getDate();
+
+  let output =
+    (m < 10 ? "0" : "") + m + "/" + (d < 10 ? "0" : "") + d + "/" + y;
+
+  color();
+
+  let container = $(".container");
+  let time = $("#currentDay").text(currentDate);
+
+
+  
 
 
 
+  //populate time blocks
+  for (i = 0; i < timeArray.length; i++) {
+    let timeBlock = $("<div>").addClass("time-block");
+    $(timeBlock).attr("data",i+9);
+    let row = $("<div>").addClass("row");
+    let hour = $("<div>").addClass("hour col-2");
+    $(hour).text(timeArray[i]);
+    let past = $("<input>").addClass("past col-8");
+    let saveBtn = $("<button>").addClass("saveBtn col-2");
+    $(saveBtn).text("Save");
+    $(row).append(hour);
+    $(row).append(past);
+    $(row).append(saveBtn);
+    $(timeBlock).append(row);
+    $(container).append(timeBlock);
 
-        let save = $(saveBtn);
-    save.click = saveData;
+    
+    for (let i = 0; i < timeArray.length + 9; i++) {
+      var local = localStorage.getItem($(past));
 
-    function saveData(){
-    var input = $(past);
-    localStorage.setItem("server", input.value);
-    var storedValue = localStorage.getItem("server");
-    console.log(storedValue);
-}
+      if (local) {
+        $(past).find("textarea").val(local);
+      }
     }
-    
+// save the plans into local memory
+    $(saveBtn).on("click", saveNotes);
+
+    function saveNotes(event) {
+      console.log("*****", event.target);
+      var timeText = $(event.target).siblings("input").val();
+      var id = $(event.target).closest(".time-block").attr("data");
+      localStorage.setItem(`hour`, timeText);
+      
+      console.log('hour')
+    }
 
    
-
-})
-
-
-let currentHour = parseInt(moment().format('H'));
-let hour=past=$("<input>").addClass("past col-8")
-console.log(currentHour);
-if(currentHour==hour){
- changeClass();
+  }
+});
+// change colors
+function color() {
+  let hour = new Date();
+  h = hour.getHours();
+  $(hour).each(function () {
+    var thisHour = parseInt($(this).attr("id"));
+  });
 }
-
-function changeClass(){
-    document.getElementById(past).className=present;
-}
-
-
